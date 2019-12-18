ebf150d049b347b04e8b9d304a26e8b79f0d3186
ysp313 <jarryves07@gmail.com>
2019-12-18 10:26:44 +0100

    Generate server

---
+++ src/test/java/com/sevenseals/sevenseals/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sevenseals.sevenseals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
